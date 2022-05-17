1118f4254f03c408b98b80303e269b78343a2568
illusionMachine <rybakovdev@yandex.ru>
2022-05-17 22:39:35 +0300

    ts type scss

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,15 @@ import { Header } from './shared/Header'
 import { Layout } from './shared/Layout'
 
 import './App.global.css'
-import { GenericList } from './shared/GenericList'
-import { generateId, generateRandomString } from './utils/react/generateRandomIndex'
-import { merge } from './utils/js/merge'
-
-const LIST = [
-  { text: 'some' },
-  { text: 'other some' },
-  { text: 'other some 2' },
-].map(generateId)
-
 
 
 export const AppComponent = (): JSX.Element => {
-  const [List, setList] = useState(LIST)
-
-  const removeItemClick = (id: string) => {
-    setList(List.filter(item => item.id !== id))
-  }
-
-  const addItemClick = () => {
-    setList(List.concat(generateId({ text: generateRandomString() })))
-  }
 
   return (
     <Layout>
       <Header />
       <Content>
         <CardsList />
-        <button onClick={addItemClick}>Add test</button>
-        {/* <GenericList list={List.map(merge({ onClick: removeItemClick }))} /> */}
       </Content>
     </Layout>
   )
