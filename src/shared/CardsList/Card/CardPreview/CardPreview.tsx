9f3bd47b68f4fe77fd482646b721482fee09399a
illusionMachine <rybakovdev@yandex.ru>
2022-05-22 13:36:26 +0300

    done posts

--- src/shared/CardsList/Card/CardPreview/CardPreview.tsx
+++ src/shared/CardsList/Card/CardPreview/CardPreview.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import styles from './cardpreview.css';
 
-export function CardPreview() {
+interface ICardPreview {
+  thumbnail: string
+}
+
+const IMG = 'https://upload.wikimedia.org/wikipedia/commons/d/dc/No_Preview_image_2.png'
+
+export function CardPreview({ thumbnail }: ICardPreview): JSX.Element {
   return (
     <div className={styles.preview}>
+
       <img
         className={styles.previewImg}
-        src="https://cdn.dribbble.com/users/1763872/screenshots/18171777/media/a636581ee85a9d5fa7e63d35f93dc0b3.jpg?compress=1&resize=1200x900&vertical=top"
+        src={!thumbnail.includes('http') ? IMG : thumbnail}
         alt="preview"
       />
+
     </div>
   );
 }
+
