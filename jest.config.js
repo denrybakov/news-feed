0977915cc3b5f63de4efe20718801c6508b189db
denrybakov <rybakovdev@yandex.ru>
2022-12-18 17:05:44 +0300

    test

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    "\\.(css)": "identity-obj-proxy",
+    "\\.(css)": "identity-obj-proxy"
   },
   snapshotSerializers: ['enzyme-to-json/serializer']
 };
